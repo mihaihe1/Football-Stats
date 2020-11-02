fb1f0a697242710d4831051c2e40f2b598ced6f3
mihai <hernest001@gmail.com>
2020-11-02 20:05:24 +0200

    added standings table(ttk)

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from bs4 import BeautifulSoup
+from future.moves import tkinter
+from tkinter import *
+from tkinter import ttk
+# from tkinter.ttk import *
 import requests
 
 goalkeeperPosition = ["GK"]
@@ -17,8 +21,9 @@ class Person:
 
 class Player(Person):
 
-    def __init__(self, name, age, nationality, minutesPlayed, goals, assists, positions=None):
+    def __init__(self, name, age, nationality, squadName, minutesPlayed, goals, assists, positions=None):
         super().__init__(name, age, nationality)
+        self.squadName = squadName
         self.minutesPlayed = minutesPlayed
         self.goals = goals
         self.assists = assists
@@ -99,9 +104,6 @@ for i in range(len(rowsStandings)):
     squadList[i].setLink('https://fbref.com'+link['href'])
     #print(rowsStandings[i].td.a.text + ": " + wins.text + " W")
 
-def printTable():
-    for obj in squadList:
-        print(obj.currentRank, obj.name, obj.wins, obj.losses, obj.goalsFor, obj.points, sep=' ')
 
 playersList = []
 managerList = []
@@ -117,6 +119,7 @@ for obj in squadList:
             playerName = rowsSquad[i].th.a.text
             playerAge = int(rowsSquad[i].find('td', attrs={'data-stat': 'age'}).text.split('-')[0])
             playerNationality = rowsSquad[i].find('td', attrs={'data-stat': 'nationality'}).a.span.text.split()[1]
+            playerSquad = search
             playerMP = rowsSquad[i].find('td', attrs={'data-stat': 'minutes'}).text
             playerGoals = rowsSquad[i].find('td', attrs={'data-stat': 'goals'}).text
             if len(playerGoals) == 0:
@@ -124,37 +127,99 @@ for obj in squadList:
             playerAssists = rowsSquad[i].find('td', attrs={'data-stat': 'assists'}).text
             playerPositions = rowsSquad[i].find('td', attrs={'data-stat': 'position'}).text.split(",")
 
-            playersList.append(Player(playerName, playerAge, playerNationality, playerMP, playerGoals, playerAssists,
+            playersList.append(Player(playerName, playerAge, playerNationality, playerSquad, playerMP, playerGoals, playerAssists,
                                       playerPositions))
 
-        sourceManager = requests.get("https://www.thesackrace.com/managers/premier-league").text
-        soupManager = BeautifulSoup(sourceManager, 'lxml')
-        divManager = soupManager.find_all('div', attrs={'class': 'container'})[4]
-        #print(divManager)
-        div2 = divManager.find('div', attrs={'class': 'breaking-news'})
-        #print(div2.prettify())
-        div3 = div2.find('div', attrs={'id': 'premier-league'})
-        #print(div3)
-        rowsManager = div3.find_all('div', attrs={'class': 'job-man'})
-        #print(rowsManager)
 
-        for j in range(len(rowsManager)):
-            club = rowsManager[j].find('h2').text
-            managerName = rowsManager[j].find('h1').text
-            paragraphs = rowsManager[j].find_all('p')
-            parAge = paragraphs[0].text.split()[1]
-            parTime = paragraphs[1].text.split(':')[1].lstrip()
-            managerList.append(Manager(managerName, parAge, "ENG", club, parTime))
+sourceManager = requests.get("https://www.thesackrace.com/managers/premier-league").text
+soupManager = BeautifulSoup(sourceManager, 'lxml')
+divManager = soupManager.find_all('div', attrs={'class': 'container'})[4]
+#print(divManager)
+div2 = divManager.find('div', attrs={'class': 'breaking-news'})
+#print(div2.prettify())
+div3 = div2.find('div', attrs={'id': 'premier-league'})
+#print(div3)
+rowsManager = div3.find_all('div', attrs={'class': 'job-man'})
+#print(rowsManager)
+
+for j in range(len(rowsManager)):
+    club = rowsManager[j].find('h2').text
+    managerName = rowsManager[j].find('h1').text
+    paragraphs = rowsManager[j].find_all('p')
+    parAge = paragraphs[0].text.split()[1]
+    parTime = paragraphs[1].text.split(':')[1].lstrip()
+    managerList.append(Manager(managerName, parAge, "ENG", club, parTime))
 
 
 def printPlayers():
     for obj in playersList:
         print(obj.name, obj.age, obj.nationality, obj.minutesPlayed, obj.goals, obj.positions)
 
-#printPlayers()
 
 def printManagers():
     for obj in managerList:
         print(obj.name, obj.age, obj.nationality, obj.club, obj.debut)
 
-printManagers()
+
+def printTable():
+    for obj in squadList:
+        print(obj.currentRank, obj.name, obj.wins, obj.losses, obj.goalsFor, obj.points, sep=' ')
+
+# printManagers()
+# printPlayers()
+# printTable()
+root = Tk()
+
+test = Label(root, text="Hello", width=10, height=10)
+test.pack()
+
+entry = Entry(root, width=20)
+entry.pack()
+
+def click():
+    label = Label(root, text=entry.get())
+    # label.pack()
+    print(label['text'])
+
+button = Button(root, command=click)
+button.pack()
+
+root.title('Football Standings')
+root.geometry("1000x1000")
+
+my_tree = ttk.Treeview(root)
+my_tree['columns'] = ("Rank", "Name", "MatchesPlayed", "Wins", "Draws", "Losses", "GoalsFor", "GoalsAgainst", "Points")
+
+my_tree.column("#0",width=0)
+my_tree.column("Rank", width=50, minwidth=25)
+my_tree.column("Name", anchor=W, width=130)
+my_tree.column("MatchesPlayed", anchor=CENTER, width=130)
+my_tree.column("Wins", anchor=W, width=50)
+my_tree.column("Draws", anchor=W, width=50)
+my_tree.column("Losses", anchor=W, width=60)
+my_tree.column("GoalsFor", anchor=W, width=100)
+my_tree.column("GoalsAgainst", anchor=W, width=120)
+my_tree.column("Points", anchor=W, width=100)
+
+
+my_tree.heading("Rank", text="Rank", anchor=W)
+my_tree.heading("Name", text="Name", anchor=W)
+my_tree.heading("MatchesPlayed", text="Matches Played", anchor=CENTER)
+my_tree.heading("Wins", text="Wins", anchor=W)
+my_tree.heading("Draws", text="Draws", anchor=W)
+my_tree.heading("Losses", text="Losses", anchor=W)
+my_tree.heading("GoalsFor", text="Goals For", anchor=W)
+my_tree.heading("GoalsAgainst", text="Goals Against", anchor=W)
+my_tree.heading("Points", text="Points", anchor=W)
+
+
+# #my_tree.insert(parent='', index='end', iid=0, text= "Parent", values=("John", 1, "ggg"))
+count = 0
+for obj in squadList:
+    my_tree.insert(parent='', index='end', iid=count, text ='', values=(obj.currentRank, obj.name, obj.matchesPlayed,
+                                                                    obj.wins, obj.draws, obj.losses, obj.goalsFor,
+                                                                    obj.goalsAgainst, obj.points))
+    count += 1
+
+my_tree.pack()
+root.mainloop()
